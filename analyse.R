a79d7fee7a96ad3fc0b3dabd1f84cbb504d637a0
Revol Eliot <revol@insa-20928>
2025-01-15 10:51:23 +0100

    beau

--- analyse.R
+++ analyse.R
@@ -1,62 +1,44 @@
 library(tidyverse)
 library(dplyr)
+library(ggrepel)
+library(randomForest)
+library(caret)
 
+############################## Pré-traitement des données ###########################################################
+
+#Importation des données 
 footards <- read.csv2(file = "~/Documents/projet_R/final_data.csv", header = TRUE, sep = ',', dec = ',')
 
+#Suppression des données N/A du dataset
 footards <- footards[! is.na(footards$current_value), ]
 
+#Conversion des données
 tibble(footards) %>% 
   mutate(height = as.integer(height)) %>%
   mutate(goals = as.numeric(goals)) %>%
   mutate(age = as.integer(age))-> footards
 
+#####################################################################################################################
 
 
 
-# distribution de la taille des joueurs de footsz
+
+############################## Analyse des données ##################################################################
+
+##### Première Partie #####
+
+# Analyse de la distribution de la taille des joueurs de foot
 footards %>% ungroup %>%
   ggplot(aes(height)) + geom_density()
 
+# Même chose en histogramme
 footards %>%
   ggplot(aes(x = height)) +
   geom_histogram(binwidth = 5, fill = "blue", color = "black", alpha = 0.7) 
-#labs(title = "Répartition des tailles des joueurs de foot", x = "Taille (en cm)", y = "Fréquence")
 
-#On peut essayer de faire l'analyse pour le gardien, l'ailier et le défenseur pour voir les différences (voir à la fin je l'ai fais)
+# On fais la même chose en fonction du poste de chaque joueur
 
-mean(footards$height)
-
-
-
-# value en fonction de l'âge + nombre de awards
-footards %>%
-  ungroup %>%
-  ggplot(aes(x=age, y=current_value, color=award))+ 
-  scale_color_gradient(low = "gold",medium="red", high = "darkgreen") +
-  geom_point(alpha=0.9) + 
-  geom_vline(aes(xintercept = median(age, na.rm = TRUE)), color = "red", linetype = "dashed", size = 1) + 
-  theme_minimal()
-
-footards %>%
-  ungroup() %>%
-  ggplot(aes(x = age, y = current_value, color = award)) + 
-  geom_point(alpha = 0.8) + 
-  geom_vline(aes(xintercept = median(age, na.rm = TRUE)), color = "red", linetype = "dashed", size = 1) + 
-  scale_color_gradientn(
-    colors = c("gold", "magenta", "blue","black"),  # Définir les couleurs pour les différents segments
-    values = scales::rescale(c(0, 10, max(footards$award, na.rm = TRUE))),  # Assigner les valeurs aux couleurs
-  ) + 
-  scale_y_continuous(labels = scales::comma_format()) +  # Formatage de l'axe y (ex. 10,000,000) 
-  labs(
-    x = "Âge du joueur",  # Label pour l'axe X
-    y = "Valeur actuelle du joueur",  # Label pour l'axe Y
-  ) +
-  theme_gray()
-
-
-
-# value en fonction de la position
-# on mappe les rôle en quelques groupes principaux
+# On mappe premièrement les rôles en quelques groupes principaux
 main_roles = c('Defender', 'Attacker', 'Midfield', 'Goalkeeper')
 roles = unique(footards$position)
 mapping_roles = function(role){
@@ -71,6 +53,35 @@ mapping_roles = function(role){
   }
 } # idee ameliorer en utilisant des Factor avec operation de coarse
 
+# On trace le graphique (attention très long à executer !!!!)
+footards %>% 
+  group_by(position) %>%
+  mutate(role = mapping_roles(position)) %>%
+  group_by(role) %>%
+  mutate(taille_m = mean(height)) %>%
+  group_by(role) %>%
+  ggplot(aes(x=height,fill=role,alpha=0.1)) + geom_density() +
+  geom_vline(aes(xintercept=taille_m, color=role), linetype="dashed")+
+  geom_label(aes(x = taille_m, y = 0, label = round(taille_m, 1)), 
+             color = "black", 
+             fill = "white", 
+             label.size = 0.25,  
+             label.padding = unit(0.2, "lines"), 
+             vjust = -0.5,
+             hjust = 0,  
+             size = 4) + 
+  labs( x = "Taille des joueurs", 
+        y = "Densité")  
+
+
+mean(footards$height) #ça c'est pour la data
+
+
+
+
+##### Deuxieme Partie #####
+
+# On regroupe les équipes du top 5 des championnats
 teams <- unique(footards$team)
 teams_top5 <- top_5_teams <- c("Manchester United", "West Ham United", "Leicester City", "Aston Villa", "Wolverhampton Wanderers",
                                "Southampton FC", "Brighton & Hove Albion", "Everton FC", "Nottingham Forest", "Leeds United", 
@@ -92,37 +103,48 @@ teams_top5 <- top_5_teams <- c("Manchester United", "West Ham United", "Leiceste
                                "RC Strasbourg Alsace", "Paris Saint-Germain")
 
 mapping_team = function(team) {
-   return(team %in% teams_top5) 
+  return(team %in% teams_top5) 
 }
 
-
-#pb trop de difference entre les valuers : idee de separer les petits et gros vhampionnats
+# Analyse de la valeur moyenne en fonction du poste 
+# Deux barres : une si on est dans le top 5 et une autre sinon
 footards %>%
   group_by(position) %>%
   mutate(top5 = mapping_team(team)) %>%
-  #filter(top5 == 0) %>% 
-  #filter(current_value >= 100000000) %>%
   group_by(position, top5) %>%
   summarise(avg_value = mean(current_value),top5) %>%   
   group_by(position) %>% 
   mutate(role = mapping_roles(position)) %>%
   ggplot(aes(x=reorder(role, -top5),y=avg_value,fill=top5)) +  geom_col(position="dodge") +
   scale_fill_manual(values = c("TRUE" = "gold", "FALSE" = "magenta")) +
-  scale_y_continuous(labels = scales::comma_format()) +  # Formatage de l'axe y (ex. 10,000,000)
-  labs(
-    x = "Position",  # Label pour l'axe X
-    y = "Valeur moyenne des prix de transfert des joueurs",  # Label pour l'axe Y
-    fill = "Top5",  # Légende pour top5
-  )
+  scale_y_continuous(labels = scales::comma_format()) +  
+  labs(x = "Position", y = "Valeur moyenne des prix de transfert des joueurs",fill = "Top5"  )
 
 
 
 
-# average valeur par position
+
+##### Troisème Partie #####
+
+# Valeur en fonction de l'âge du joueur et des awards qu'il a obtenu
+# Avec médiane tracée en rouge
+
+footards %>%
+  ungroup() %>%
+  ggplot(aes(x = age, y = current_value, color = award)) + 
+  geom_point(alpha = 0.8) + 
+  geom_vline(aes(xintercept = median(age, na.rm = TRUE)), color = "red", linetype = "dashed", size = 1) + 
+  scale_color_gradientn(colors = c("gold", "magenta", "blue","black"),values = scales::rescale(c(0, 10, max(footards$award, na.rm = TRUE)))) + 
+  scale_y_continuous(labels = scales::comma_format()) +
+  labs( x = "Âge du joueur", y = "Valeur actuelle du joueur") +
+  theme_gray()
 
 
 
-#rapport awards et value
+
+###### Exprerience : pas retenu car pas très intéressant au final
+
+#Rapport entre le nombre d'award et la valeur
 #Ajout d'un mapping pour "regouper" un certain nombre d'ward dans la même catégorie (exemple, ceux qui ont moins de 5 award dans le même groupe,
 #ceux qui en ont moins de 10 dans un deuxième..)
 mapping_award = function(award){
@@ -137,7 +159,7 @@ footards %>%
   ggplot(aes(x=award_5*10,y=avg_value)) + geom_point() +geom_line() 
 
 
-# value en fonction du nb de goals (pas interessant)
+#Rapport entre le nombre de but et la valeur
 footards %>%
   ungroup %>%
   ggplot(aes(x=goals, y=current_value)) + geom_point() + theme_minimal()
@@ -145,57 +167,26 @@ footards %>%
 
 
 
-# distribution de la taille des joueurs de foots en fonction de leur poste
-footards %>% 
-  group_by(position) %>%
-  mutate(role = mapping_roles(position)) %>%
-  group_by(role) %>%
-  mutate(taille_m = mean(height)) %>%
-  group_by(role) %>%
-  ggplot(aes(x=height,fill=role,alpha=0.1)) + geom_density() +
-  geom_vline(aes(xintercept=taille_m, color=role), linetype="dashed")+
-  geom_label(aes(x = taille_m, y = 0, label = round(taille_m, 1)), 
-             color = "black", 
-             fill = "white",  # Fond blanc pour le texte
-             label.size = 0.25,  # Taille du contour du label
-             label.padding = unit(0.2, "lines"),  # Espacement autour du texte
-             vjust = -0.5,  # Ajuste la position verticale du texte
-             hjust = 0,  # Ajuste la position horizontale du texte
-             size = 4) +  # Taille du texte
-  labs( x = "Taille des joueurs", 
-       y = "Densité")  # Titres et labels
 
-
-footards %>% 
-  group_by(position) %>%
-  mutate(role = mapping_roles(position)) %>%
-  ggplot(aes(x = height, fill = role, color = role, alpha = 0.1)) + 
-  geom_density()   # Tracer la densité
-  stat_summary(fun = mean,y=height, geom = "point", size = 3, shape = 21, fill = "black") +  # Ajouter le point de la moyenne
-  
-
-
-
-# on vafaire un random forest pour prédire la value d'un joueur en fonciton de tout
-library(randomForest)
-library(caret)
-
+#### Quatrième partie ####
+# Entrainement du modèle sur tous les joueurs sauf les 20 meilleurs pour test sur le modèle plus tard
 footards %>%
   arrange(highest_value) %>%
   head(10734) -> footards_nuls
-  
+
+#Partition du jeu de donnée
 set.seed(42)
 trainIndex <- createDataPartition(footards_nuls$current_value,p=0.8,list=FALSE)
-
-  
 trainData <- footards_nuls[trainIndex, ]
 testData <- footards_nuls[-trainIndex, ]
 set.seed(69)
+
+#Création du modèle
 model <- randomForest(current_value ~ age+height+goals+assists+days_injured+games_injured+award+minutes.played+appearance+position+team,data=trainData,ntree=500,importance=TRUE)
 print(model)
 
+#Calcul du score de prédiction sur le jeu de test
 prediction <- predict(model,testData)
-# Calcul de l'erreur quadratique moyenne
 print(RMSE(prediction, testData$current_value))
 print(sqrt(RMSE(prediction, testData$current_value)))
 
@@ -207,7 +198,7 @@ print(r_squared <- r_squared <- 1 - (sum((testData$current_value - prediction)^2
 result <- data.frame(name=testData$name,Actual = testData$current_value,Predicted=prediction)
 
 
-# Visualization of Actual vs Predicted Transfer Values
+# Courbe des prédictions comparées aux réels valeurs
 ggplot(result, aes(y = Actual, x = Predicted)) +
   geom_point(color = "blue", alpha = 0.6) +
   geom_abline(slope = 1, intercept = 0, color = "red", linetype = "dashed") +
@@ -218,6 +209,7 @@ ggplot(result, aes(y = Actual, x = Predicted)) +
 
 varImpPlot(model)
 
+# Fonction pour réaliser une inférence
 predict_transfer_value<-function(age, height, goals, assists, days_injured, games_injured, award, minutes.played, appearance, position, team) {
   player <- data.frame(
     age = age,
@@ -239,24 +231,30 @@ predict_transfer_value<-function(age, height, goals, assists, days_injured, game
 
 # predict_transfer_value(0,0,50,60,10000000000000,5000000000000,150,50000,110,"Attack Centre-Forward","Real Madrid")
 
-# extraire les 20 joueurs les plus chers (en absolute value)
+
+
+#### Prediction des joueurs les plus chers ####
+
+# Extraction des 20 joueurs les plus chers (en absolute value)
 footards %>%
   arrange(desc(highest_value)) %>%
   head(20) -> plus_chers
-mbappe <- plus_chers[1,]
 
+# Predicton des 20 joueurs les plus cher
 pred_plus_cher <- plus_chers %>%
   mutate(pred = predict_transfer_value(age, height,goals, assists, days_injured, games_injured, award, minutes.played, appearance, position, team))
 
+# Test sur mbappé
+mbappe <- plus_chers[1,]
 print(mbappe)
 predict_transfer_value(mbappe$age, mbappe$height, mbappe$goals, mbappe$assists, mbappe$days_injured, mbappe$games_injured, mbappe$award, mbappe$minutes.played, mbappe$appearance, mbappe$position, mbappe$team)     
 
+# Plot des plus chers
 plus_chers %>%
   ggplot(aes(x = current_value)) +
   geom_histogram(binwidth = 5, fill = "blue", color = "black", alpha = 0.7) 
 
-library(ggrepel)
-
+# Plot des predictions vs prix réels sur les plus cher
 pred_plus_cher %>%
   ggplot(aes(y = current_value, x = pred, color = team)) +
     geom_point(color = "blue", alpha = 0.6) +
@@ -267,13 +265,13 @@ pred_plus_cher %>%
     theme_minimal()
 
 
+
+#### Creation d'une super équipe ####
+
 # Vecteur roles
 roles <- unique(footards$position)
 
-# Liste des positions à supprimer
-fils_de_pute_pollueur_de_dataset <- c("Attack", "midfield", "Defender")
-
-
+# Recuperation du meilleur 11 en supprimant les roles redodants
 footards %>%
   group_by(position) %>%
   arrange(desc(current_value)) %>%
@@ -283,16 +281,20 @@ footards %>%
 
 sum(best_team$current_value)
 
-prenoms[which(prenoms$Prénom %in% fm),]
-
 equipes_top <- unique(best_team$team)
 best_team_prepared <- best_team %>% mutate(team = 'DreamTeam FC')
 equipes_top_peuplees <- rbind(footards[which(footards$team %in% equipes_top),], best_team_prepared)
 
+# Représentation de la valeur moyenne des joueurs des meilleurs équipes vs notre super équipe
 equipes_top_peuplees %>%
+  mutate(current_value = as.double(current_value)) %>%
+  select(team, current_value) %>%
   group_by(team) %>%
-  mutate(mean_val = mean(current_value)) %>%
+  summarize(mean_val = mean(current_value)) %>%
   ungroup() %>%
-  ggplot(aes(x= reorder(team, mean_val), y=mean_val)) +geom_col() + 
-  theme_minimal() +  coord_flip()
+  ggplot(aes(x=mean_val, y=fct_reorder(team, mean_val), fill=team)) +geom_col() + 
+  labs(y = "Team",
+       x = "Average player value") +
+  theme_minimal()
 
+#####################################################################################################################
